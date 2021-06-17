940d78c085d6e2b3c0791a252811a19a77ec15ba
dharanisekar1999 <dharanisekar9@gmail.com>
2021-06-17 20:13:43 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCOSW-VmwX7q1IDXyxtXqTd4NmNFlTkDlk",
+    authDomain: "github-searcher-4a8bd.firebaseapp.com",
+    projectId: "github-searcher-4a8bd",
+    storageBucket: "github-searcher-4a8bd.appspot.com",
+    messagingSenderId: "533546952241",
+    appId: "1:533546952241:web:99c1bfa43992d46f141710"
+  }
 };
